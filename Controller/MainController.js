a1502a85050594b0694c64218b995f4240bf9af1
Fazzina <fabio.leoni@edu.itspiemonte.it>
2023-04-12 11:55:04 +0200

    test

--- Controller/MainController.js
+++ Controller/MainController.js
@@ -1,10 +1,43 @@
 angular.module("myApp", []).controller("MainController", [
   "$scope",
   function ($scope) {
-    $scope.paginaCorrente = "Directives/selfassessment.html";
+    $scope.textSelfassessment =
+      "Il Self Assessment è lo strumento che valuta il tuo impegno sui temi HSE (Sicurezza, Salute e Ambiente). È costituito da 5 moduli, ognuno dei quali si concentra su un obiettivo differente. Ti basteranno 30 minuti per completare il Self Assessment. Puoi salvare i tuoi progressi in qualsiasi momento e tornare più tardi.";
+    $scope.textApprofondimento =
+      "Il Self Assessment si concentra sui comportamenti che dovresti adottare per rendere più sicuro e sostenibile il tuo lavoro e quello di chi ti sta intorno.Le domande ti aiutano a comprendere come è meglio agire al fine di evitare incidenti, gestire il rapporto con i fornitori o, ad esempio, definire competenze e responsabilità.";
 
-    $scope.changePage = function (pagina) {
-      $scope.paginaCorrente = pagina;
+    $scope.showSelfAssessment = true;
+    $scope.showApprofondimento = false;
+    $scope.showArchivio = false;
+
+    $scope.switchSelfAppr = function () {
+      if ($scope.showSelfAssessment == true) {
+        $scope.showSelfAssessment = false;
+        $scope.showApprofondimento = true;
+      } else if ($scope.showApprofondimento == true) {
+        $scope.showSelfAssessment = true;
+        $scope.showApprofondimento = false;
+      }
+    };
+
+    $scope.switchSelfArch = function () {
+      if ($scope.showSelfAssessment == true) {
+        $scope.showSelfAssessment = false;
+        $scope.showArchivio = true;
+      } else if ($scope.showArchivio == true) {
+        $scope.showSelfAssessment = true;
+        $scope.showArchivio = false;
+      }
+    };
+
+    $scope.switchApprArch = function () {
+      if ($scope.showApprofondimento == true) {
+        $scope.showApprofondimento = false;
+        $scope.showArchivio = true;
+      } else if ($scope.showArchivio == true) {
+        $scope.showApprofondimento = true;
+        $scope.showArchivio = false;
+      }
     };
   },
 ]);
